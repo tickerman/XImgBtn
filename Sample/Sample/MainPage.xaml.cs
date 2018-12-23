ae4844cf2db32859da7683bed22141df569f5da6
xigmond berton <xigmond@gmail.com>
2018-12-23 20:01:53 +0330

    Sample Project Added

---
+++ Sample/Sample/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms;
+using XImgBtn;
+
+namespace Sample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            xib1.BtnTapped += BtnTapped;
+        }
+
+        private void BtnTapped(object sender, XIBEventArgs args)
+        {
+            statusLabel.Text = $"Button Pressed Id:{args.XId} , String Parameter:{args.XMsg}";
+
+            //switch (id)
+            //{
+            //    case 1:
+            //        // ToDo ...
+            //        break;
+            //    .
+            //    .
+            //    .
+            //
+            //    default:
+            //        // ToDo ...
+            //        break;
+            //}
+        }
+    }
+}
